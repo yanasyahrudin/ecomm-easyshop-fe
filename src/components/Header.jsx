9e2f1fa843a95a39566c8bb9193ac006814485e8
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-10-12 06:28:53 +0700

    header design tailwind css react part 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,11 @@ import {
   FaUser,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const {pathname} = useLocation()
+  
   const [showSidebar, setShowSidebar] = useState(true);
   const user = false;
 
@@ -93,6 +96,10 @@ const Header = () => {
       <div className="w-white">
         <div className="w-[85%] lg:w-[90%] mx-auto">
           <div className="h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap">
+            <div className="md-lg:w-full w-3/12 md-lg:pt-4">
+            <div className="flex justify-between items-center">
+
+
             <Link to="/">
               <img src="http://localhost:3000/images/logo.png" alt="" />
             </Link>
@@ -101,6 +108,39 @@ const Header = () => {
                 <FaList />
               </span>
             </div>
+            </div>
+            </div>
+
+                <div className="md:lg:w-full w-9/12">
+                <div className="flex justify-between md-lg:justify items-center flex-wrap pl-8">
+                  <ul className="flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden">
+                    <li>
+                      <Link className={`p-2 block ${pathname ==='/'? 'text-[#059473]':'text-slate-600'}`}>Home</Link>
+                    </li>
+
+                    <li>
+                      <Link className={`p-2 block ${pathname ==='/shop'? 'text-[#059473]':'text-slate-600'}`}>Shop</Link>
+                    </li>
+
+                    <li>
+                      <Link className={`p-2 block ${pathname ==='/blog'? 'text-[#059473]':'text-slate-600'}`}>Blog</Link>
+                    </li>
+                    <li>
+                      <Link className={`p-2 block ${pathname ==='/about'? 'text-[#059473]':'text-slate-600'}`}>About Us</Link>
+                    </li>
+                    <li>
+                      <Link className={`p-2 block ${pathname ==='/contact'? 'text-[#059473]':'text-slate-600'}`}>Contact Us</Link>
+                    </li>
+
+                  </ul>
+
+                </div>
+
+                </div>
+
+
+
+
           </div>
         </div>
       </div>
