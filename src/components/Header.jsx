e0c5138e3aa303c90fd9cd5fca6d6729729205d2
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-10-16 06:16:44 +0700

    header design tailwind css react part 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { IoMdArrowDropdown, IoMdPhonePortrait } from "react-icons/io";
 import {
   FaFacebookF,
   FaGithub,
+  FaHeart,
   FaLinkedin,
   FaList,
   FaLock,
@@ -12,12 +13,15 @@ import {
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { FaCartShopping } from 'react-icons/fa6';
 
 const Header = () => {
-  const {pathname} = useLocation()
-  
+  const { pathname } = useLocation();
+
   const [showSidebar, setShowSidebar] = useState(true);
   const user = false;
+
+  const whishlist_count = 3;
 
   return (
     <div className="w-full bg-white">
@@ -97,54 +101,109 @@ const Header = () => {
         <div className="w-[85%] lg:w-[90%] mx-auto">
           <div className="h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap">
             <div className="md-lg:w-full w-3/12 md-lg:pt-4">
-            <div className="flex justify-between items-center">
-
-
-            <Link to="/">
-              <img src="http://localhost:3000/images/logo.png" alt="" />
-            </Link>
-            <div className="justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden" onClick={()=>setShowSidebar(false)}>
-              <span>
-                <FaList />
-              </span>
-            </div>
-            </div>
-            </div>
-
-                <div className="md:lg:w-full w-9/12">
-                <div className="flex justify-between md-lg:justify items-center flex-wrap pl-8">
-                  <ul className="flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden">
-                    <li>
-                      <Link className={`p-2 block ${pathname ==='/'? 'text-[#059473]':'text-slate-600'}`}>Home</Link>
-                    </li>
-
-                    <li>
-                      <Link className={`p-2 block ${pathname ==='/shop'? 'text-[#059473]':'text-slate-600'}`}>Shop</Link>
-                    </li>
-
-                    <li>
-                      <Link className={`p-2 block ${pathname ==='/blog'? 'text-[#059473]':'text-slate-600'}`}>Blog</Link>
-                    </li>
-                    <li>
-                      <Link className={`p-2 block ${pathname ==='/about'? 'text-[#059473]':'text-slate-600'}`}>About Us</Link>
-                    </li>
-                    <li>
-                      <Link className={`p-2 block ${pathname ==='/contact'? 'text-[#059473]':'text-slate-600'}`}>Contact Us</Link>
-                    </li>
-
-                  </ul>
-
+              <div className="flex justify-between items-center">
+                <Link to="/">
+                  <img src="http://localhost:3000/images/logo.png" alt="" />
+                </Link>
+                <div
+                  className="justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden"
+                  onClick={() => setShowSidebar(false)}
+                >
+                  <span>
+                    <FaList />
+                  </span>
                 </div>
+              </div>
+            </div>
 
+            <div className="md:lg:w-full w-9/12">
+              <div className="flex justify-between md-lg:justify items-center flex-wrap pl-8">
+                <ul className="flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden">
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/" ? "text-[#059473]" : "text-slate-600"
+                      }`}
+                    >
+                      Home
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/shop"
+                          ? "text-[#059473]"
+                          : "text-slate-600"
+                      }`}
+                    >
+                      Shop
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/blog"
+                          ? "text-[#059473]"
+                          : "text-slate-600"
+                      }`}
+                    >
+                      Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/about"
+                          ? "text-[#059473]"
+                          : "text-slate-600"
+                      }`}
+                    >
+                      About Us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/contact"
+                          ? "text-[#059473]"
+                          : "text-slate-600"
+                      }`}
+                    >
+                      Contact Us
+                    </Link>
+                  </li>
+                </ul>
+
+                <div className="flex md-lg:hidden justify-center items-center gap-5">
+                  <div className="flex justify-center gap-5">
+                    <div className="relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]">
+                      <span className="text-xl text-green-500">
+                        <FaHeart />
+                      </span>
+                      <div className="w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]">
+                        {whishlist_count}
+                      </div>
+                    </div>
+
+                    <div className="relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]">
+                      <span className="text-xl text-green-500">
+                        <FaCartShopping />
+                      </span>
+                      <div className="w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]">
+                        {whishlist_count}
+                      </div>
+                    </div>
+
+
+                  </div>
                 </div>
-
-
-
-
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 };
