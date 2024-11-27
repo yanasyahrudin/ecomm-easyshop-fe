d92096e49c0b58256dfcb365f5292e21100fe3f3
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-11-27 16:03:51 +0700

    setup latest product part 2

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,57 +1,94 @@
-import React from 'react';
+import React from "react";
+
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Products = () => {
-    const products = [[1,2,3], [4,5,6], [7,8,9], ]
+const Products = ({ title }) => {
+  const products = [
+    [1, 2, 3],
+    [4, 5, 6],
+  ];
 
-    const responsive = {
-        superLargeDesktop: {
-          breakpoint: { max: 4000, min: 3000 },
-          items: 1,
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 1,
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 1,
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-        },
-      };
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-      const ButtonGroup = ()=>{
-        
-      }
+  const ButtonGroup = ({ next, previous }) => {
     return (
-        <div className='flex gap-8'>
-            <Carousel
-                autoPlay={false}
-                infinite={false}
-                arrows={false}
-                responsive={responsive}
-                transitionDuration={500}
-                renderButtonGroupOutside={true}
-                customButtonGroup={ButtonGroup}
-              >
-                <div className='flex flex-col justify-start gap-2'>
-                    <Link to='#'>
-                    <img src="" alt="" />
-                    <div className='px-3 flex justify-start items-start gap-1 flex-col text-slate-600'>
-                        <h2>Product Name</h2>
-                        <span>$343</span>
-
-                    </div>
-                    </Link>
-                </div>
-                </Carousel>
+      <div className="flex justify-between items-center">
+        <div className="text-xl font-bold text-slate-600">{title}</div>
+        <div className="flex justify-center items-center gap-3 text-slate-600">
+          <button
+            onClick={() => previous()}
+            className="w-[30px] h-[30px] flex justify-center items-center bg-slate-300 border border-slate-200"
+          >
+            <IoIosArrowBack />
+          </button>
+          <button
+            onClick={() => next()}
+            className="w-[30px] h-[30px] flex justify-center items-center bg-slate-300 border border-slate-200"
+          >
+            <IoIosArrowForward />
+          </button>
         </div>
+      </div>
     );
+  };
+
+
+  return (
+    <div className="flex gap-8 flex-col-reverse">
+      <Carousel
+        autoPlay={false}
+        infinite={false}
+        arrows={false}
+        responsive={responsive}
+        transitionDuration={500}
+        renderButtonGroupOutside={true}
+        customButtonGroup={<ButtonGroup/>}
+      >
+
+        {products.map((p, i) => {
+          return (
+            <div className="flex flex-col justify-start gap-2">
+              {p.map((pl, j) => (
+                <div className="flex flex-col justify-start gap-2">
+                  <Link className="flex justify-start items-start" to="#">
+                    <img
+                      className="w-[110px] h-[110px]"
+                      src={`http://localhost:3000/images/products/${pl}.webp`}
+                      alt=""
+                    />
+                    <div className="px-3 flex justify-start items-start gap-1 flex-col text-slate-600">
+                      <h2>Product Name</h2>
+                      <span className="text-lg font-bold">$343</span>
+                    </div>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
 };
 
 export default Products;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Header from "../components/Header";
-import Banner from './../components/Banner';
+import Banner from "./../components/Banner";
 import Categorys from "../components/Categorys";
 import FeeatureProducts from "../components/products/FeeatureProducts";
-import Products from '../components/products/Products';
+import Products from "../components/products/Products";
 
 const Home = () => {
   return (
@@ -13,27 +13,21 @@ const Home = () => {
       <Categorys />
       <div className="py-[45px]">
         <FeeatureProducts />
-
       </div>
       <div className="py-10">
-      <div className="w-[85%] flex flex-wrap mx-auto">
+        <div className="w-[85%] flex flex-wrap mx-auto">
           <div className="grid w-full grid-cols-3 md-lg:grid-cols-2 md:grid-cols-1 gap-7">
             <div className="overflow-hidden">
-
-              <Products />
+              <Products title="Latest Product" />
             </div>
             <div className="overflow-hidden">
-
-              <Products />
+              <Products title="Top Rated Product" />
             </div>
             <div className="overflow-hidden">
-
-              <Products />
+              <Products title="Discount Product" />
             </div>
-
           </div>
-
-      </div>
+        </div>
       </div>
     </div>
   );
