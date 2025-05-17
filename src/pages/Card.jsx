093ec1353d3a6c4ae2c04aecbd9cbdec74dd510c
yanasyahrudin <yanna6710@gmail.com>
2025-05-17 11:59:10 +0700

    Card Page Design Setup Part 4

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -102,54 +102,84 @@ const Card = () => {
                           </h2>
                         </div>
 
-                        <div className='bg-white p-4'>
+                        <div className="bg-white p-4">
                           {[1].map((p, i) => (
-                          <div className="w-full flex flex-wrap">
-                            <div className="flex sm:w-full gap-2 w-7/12">
-                              <div className="flex gap-2 justify-start items-center">
-                                <img
-                                  className="w-[80px] h-[80px]"
-                                  src={`http://localhost:3000/images/products/${
-                                    i + 1
-                                  }.webp`}
-                                  alt=""
-                                />
-                                <div className="pr-4 text-slate-600">
-                                  <h2 className="text-md font-semibold">
-                                    Product Name
+                            <div className="w-full flex flex-wrap">
+                              <div className="flex sm:w-full gap-2 w-7/12">
+                                <div className="flex gap-2 justify-start items-center">
+                                  <img
+                                    className="w-[80px] h-[80px]"
+                                    src={`http://localhost:3000/images/products/${
+                                      i + 1
+                                    }.webp`}
+                                    alt=""
+                                  />
+                                  <div className="pr-4 text-slate-600">
+                                    <h2 className="text-md font-semibold">
+                                      Product Name
+                                    </h2>
+                                    <span className="">Brand: Jara</span>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <div className="flex justify-between w-5/12 sm:w-full sm:mt-3">
+                                <div className="pl-4 sm:pl-0">
+                                  <h2 className="text-lg text-orange-500">
+                                    $240
                                   </h2>
-                                  <span className="">Brand: Jara</span>
+                                  <p className="line-through">$300</p>
+                                  <p>-$15</p>
+                                </div>
+
+                                <div className="flex gap-2 flex-col">
+                                  <div className="flex bg-slate-200 h-[30px] justify-center text-xl">
+                                    <div className="px-3 cursor-pointer">-</div>
+                                    <div className="px-3">2</div>
+                                    <div className="px-3 cursor-pointer">+</div>
+                                  </div>
+                                  <button className="px-5 py-[3px] bg-red-500">
+                                    Delete
+                                  </button>
                                 </div>
                               </div>
                             </div>
-
-                            <div className="flex justify-between w-5/12 sm:w-full sm:mt-3">
-                              <div className="pl-4 sm:pl-0">
-                                <h2 className="text-lg text-orange-500">
-                                  $240
-                                </h2>
-                                <p className="line-through">$300</p>
-                                <p>-$15</p>
-                              </div>
-
-                              <div className="flex gap-2 flex-col">
-                                <div className="flex bg-slate-200 h-[30px] justify-center text-xl">
-                                  <div className="px-3 cursor-pointer">-</div>
-                                  <div className="px-3">2</div>
-                                  <div className="px-3 cursor-pointer">+</div>
-                                </div>
-                                <button className="px-5 py-[3px] bg-red-500">
-                                  Delete
-                                </button>
-                              </div>
-                            </div>
-                          </div>
-                        ))}
+                          ))}
                         </div>
                       </div>
                     )}
+                  </div>
+                </div>
 
-
+                <div className="w-[33%] md-lg:w-full">
+                  <div className="pl-3 md-lg:pl-0 md-lg:mt-5">
+                    {card_products.length > 0 && (
+                      <div
+                        className="bg-white p-3
+                            text-slate-600 flex flex-col gap-3"
+                      >
+                        <h2 className="text-xl font-bold ">Order Summary</h2>
+                        <div className="flex justify-between items-center">
+                          <span>2 Items</span>
+                          <span>$343</span>
+                        </div>
+                        <div className="flex justify-between items-center">
+                          <span>Shipping Fee</span>
+                          <span>$40</span>
+                        </div>
+                        <div className="flex gap-2">
+                          <input className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-green-500 rounded-sm " type='text' placeholder="Input Voucher Coupon"/>
+                        <button className="px-5 py-[1px] bg-[#059473] text-white rounded-sm uppercase text-sm">Apply</button>
+                        </div>
+                        <div className="flex justify-between items-center">
+                          <span>Total</span>
+                          <span className="text-lg text-[#059473]">$430</span>
+                        </div>
+                        <button className="px-5 py-[6px] rounded-sm hover:shadow-red-500/50 hover:shadow-lg bg-red-500 text-sm text-white uppercase">
+                          Process to Checkout
+                        </button>
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
