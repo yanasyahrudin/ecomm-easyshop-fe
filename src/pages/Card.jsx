cc27feb0ac6da0b0fbc1c06c07f270d412eb291b
yanasyahrudin <yanna6710@gmail.com>
2025-05-17 16:06:37 +0700

    Shipping Page Design Setup Part 1

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
 
 const Card = () => {
+  const navigate = useNavigate();
   const card_products = [1, 2];
   const outOfStockProduct = [1, 2];
+
+  const redirect = () => {
+    navigate("/shipping", {
+      state: {
+        products: [],
+        price: 500,
+        shipping_fee: 40,
+        items: 2,
+      },
+    });
+  };
 
   return (
     <div>
@@ -150,37 +162,45 @@ const Card = () => {
                     )}
                   </div>
                 </div>
-
-                <div className="w-[33%] md-lg:w-full">
-                  <div className="pl-3 md-lg:pl-0 md-lg:mt-5">
-                    {card_products.length > 0 && (
-                      <div
-                        className="bg-white p-3
+              </div>
+              <div className="w-[33%] md-lg:w-full">
+                <div className="pl-3 md-lg:pl-0 md-lg:mt-5">
+                  {card_products.length > 0 && (
+                    <div
+                      className="bg-white p-3
                             text-slate-600 flex flex-col gap-3"
-                      >
-                        <h2 className="text-xl font-bold ">Order Summary</h2>
-                        <div className="flex justify-between items-center">
-                          <span>2 Items</span>
-                          <span>$343</span>
-                        </div>
-                        <div className="flex justify-between items-center">
-                          <span>Shipping Fee</span>
-                          <span>$40</span>
-                        </div>
-                        <div className="flex gap-2">
-                          <input className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-green-500 rounded-sm " type='text' placeholder="Input Voucher Coupon"/>
-                        <button className="px-5 py-[1px] bg-[#059473] text-white rounded-sm uppercase text-sm">Apply</button>
-                        </div>
-                        <div className="flex justify-between items-center">
-                          <span>Total</span>
-                          <span className="text-lg text-[#059473]">$430</span>
-                        </div>
-                        <button className="px-5 py-[6px] rounded-sm hover:shadow-red-500/50 hover:shadow-lg bg-red-500 text-sm text-white uppercase">
-                          Process to Checkout
+                    >
+                      <h2 className="text-xl font-bold ">Order Summary</h2>
+                      <div className="flex justify-between items-center">
+                        <span>2 Items</span>
+                        <span>$343</span>
+                      </div>
+                      <div className="flex justify-between items-center">
+                        <span>Shipping Fee</span>
+                        <span>$40</span>
+                      </div>
+                      <div className="flex gap-2">
+                        <input
+                          className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-green-500 rounded-sm "
+                          type="text"
+                          placeholder="Input Voucher Coupon"
+                        />
+                        <button className="px-5 py-[1px] bg-[#059473] text-white rounded-sm uppercase text-sm">
+                          Apply
                         </button>
                       </div>
-                    )}
-                  </div>
+                      <div className="flex justify-between items-center">
+                        <span>Total</span>
+                        <span className="text-lg text-[#059473]">$430</span>
+                      </div>
+                      <button
+                        onClick={redirect}
+                        className="px-5 py-[6px] rounded-sm hover:shadow-red-500/50 hover:shadow-lg bg-red-500 text-sm text-white uppercase"
+                      >
+                        Process to Checkout
+                      </button>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
