15cc2e40fbac5ef892c696a938372006a69e3b5a
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-10 09:55:18 +0700

    customer login register page design part 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Shops from "./pages/Shops";
 import Card from "./pages/Card";
 import Shipping from "./pages/Shipping";
 import Details from  "./pages/Details";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/shops" element={<Shops />} />
         <Route path="/card" element={<Card />} />
         <Route path="/shipping" element={<Shipping />} />
