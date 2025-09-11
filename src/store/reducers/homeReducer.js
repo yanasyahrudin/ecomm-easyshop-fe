97f3e63a43aec6aeeb922f9e23a74503377ac5a7
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-11 19:29:20 +0700

    category fetch and show part 1

--- src/store/reducers/homeReducer.js
+++ src/store/reducers/homeReducer.js
@@ -1,4 +1,19 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+export const get_category = createAsyncThunk(
+    "product/get_category",
+    async (_, {fulfillWithValue}) => {
+        try {
+            const {data} = await api.get("/home/get_categorys")
+            console.log(data)
+            return fulfillWithValue(data)
+        } catch (error) {
+            console.log(error.response)
+        }
+    }
+)
+//end method
 
 export const homeReducer = createSlice({
     name: "home",
