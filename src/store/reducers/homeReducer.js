0e9d8b2fd12022fcfd7fd83389d7381d918a4824
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-11 16:39:48 +0700

    redux toolkit setup

---
+++ src/store/reducers/homeReducer.js
@@ -0,0 +1,18 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const homeReducer = createSlice({
+    name: "home",
+    initialState: {
+        categorys: [],
+    },
+    reducers : {
+
+    },
+    extraReducers: (builder) => {
+
+
+    }
+
+})
+
+export default homeReducer.reducer;
