053f45680280e2ebb5706a36c4ed624f561c7c1f
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-21 09:07:27 +0700

    card product get and show part 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,10 +21,10 @@ import { FaCartShopping } from "react-icons/fa6";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-
   const navigate = useNavigate();
   const { categorys } = useSelector((state) => state.home);
-  const {userInfo} = useSelector(state=> state.auth)
+  const { userInfo } = useSelector((state) => state.auth);
+  const { card_product_count } = useSelector((state) => state.card);
 
   const { pathname } = useLocation();
 
@@ -40,6 +40,14 @@ const Header = () => {
   const search = () => {
     navigate(`/products/search?category=${category}&&value=${searchValue}`);
   };
+
+  const redirect_card_page = () => {
+    if (userInfo) {
+      navigate("/card");
+    } else {
+      navigate("/login");
+    }
+  }
 
   return (
     <div className="w-full bg-white">
@@ -206,13 +214,15 @@ const Header = () => {
                       </div>
                     </div>
 
-                    <div className="relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]">
+                    <div onClick={redirect_card_page} className="relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]">
                       <span className="text-xl text-green-500">
                         <FaCartShopping />
                       </span>
-                      <div className="w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]">
-                        {whishlist_count}
-                      </div>
+                      {card_product_count !== 0 && (
+                        <div className="w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]">
+                          {card_product_count}
+                        </div>
+                      )}
                     </div>
                   </div>
                 </div>
@@ -443,7 +453,10 @@ const Header = () => {
                     id=""
                     placeholder="What do you need"
                   />
-                  <button onClick={search} className="bg-[#059473] right-0 absolute px-8 h-full font-semibold uppercase  text-white">
+                  <button
+                    onClick={search}
+                    className="bg-[#059473] right-0 absolute px-8 h-full font-semibold uppercase  text-white"
+                  >
                     Search
                   </button>
                 </div>
