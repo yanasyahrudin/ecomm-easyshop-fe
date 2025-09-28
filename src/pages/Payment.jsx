3f8cd248d425828441254fb2f11e510c014d0c3b
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-28 10:42:06 +0700

    order payment page design part 1

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useLocation } from "react-router-dom";
 
 const Payment = () => {
+  
+  const {
+    state: { price, items, orderId },
+  } = useLocation();
+  const [payment, setPaymentMethod] = useState("stripe");
+
   return (
-    <div>Payment</div>
-  )
-}   
-export default Payment
+    <div>
+      <Header />
+      <section className="bg-[#eeeeee]">
+        <div className="w-[85%] lg:w-[90%] md:w-[90%] sm:w-[90%] mx-auto py-16 mt-4">
+          <div className="flex flex-wrap md:flex-col-reverse">
+            <div className="pr-2 md:pr-0">
+              <div className="flex flex-wrap">
+                <div className="flex flex-col gap-[3px] justify-center">
+                  <img
+                    src="http://localhost:3000/images/payment/strip.png"
+                    alt=""
+                  />
+                </div>
+                <span className="text-slate-600">Stripe</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default Payment;
