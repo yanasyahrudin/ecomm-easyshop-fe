0400f369621b67b70a730f5d77b0f835e0c2e691
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-28 10:59:20 +0700

    order payment page design part 1

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useLocation } from "react-router-dom";
+import Stripe from "../components/Stripe";
 
 const Payment = () => {
-  
   const {
     state: { price, items, orderId },
   } = useLocation();
@@ -18,16 +18,50 @@ const Payment = () => {
           <div className="flex flex-wrap md:flex-col-reverse">
             <div className="pr-2 md:pr-0">
               <div className="flex flex-wrap">
-                <div className="flex flex-col gap-[3px] justify-center">
+                <div
+                  onClick={() => setPaymentMethod("stripe")}
+                  className={`w-[20%] border-r cursor-pointer py-8 px-12 ${
+                    payment === "stripe" ? "bg-white" : "bg-slate-100"
+                  }`}
+                >
+                  <div className="flex flex-col gap-[3px] justify-center items-center">
+                    <img
+                      src="http://localhost:3000/images/payment/strip.png"
+                      alt=""
+                    />
+                  </div>
+                  <span className="text-slate-600">Stripe</span>
+                </div>
+              </div>
+
+              <div
+                onClick={() => setPaymentMethod("cod")}
+                className={`w-[20%] border-r cursor-pointer py-8 px-12 ${
+                  payment === "stripe" ? "bg-white" : "bg-slate-100"
+                }`}
+              >
+                <div className="flex flex-col gap-[3px] justify-center items-center">
                   <img
-                    src="http://localhost:3000/images/payment/strip.png"
+                    src="http://localhost:3000/images/payment/cod.png"
                     alt=""
                   />
                 </div>
-                <span className="text-slate-600">Stripe</span>
+                <span className="text-slate-600">COD</span>
               </div>
             </div>
           </div>
+          {paymentMethod === "stripe" && (
+            <div>
+              <Stripe />
+            </div>
+          )}
+          {paymentMethod === "cod" && (
+            <div className="w-full px-4 py-8 bg-white shadow-md">
+              <button className="px-10 py-[6px] rounded-sm hover:shadow-green-500/20 hover:shadow-md bg-[#059473] text-white">
+                Pay Now
+              </button>
+            </div>
+          )}
         </div>
       </section>
       <Footer />
