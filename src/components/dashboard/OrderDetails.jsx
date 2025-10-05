eb59d14c9bec56a6c35c436c18fc678caa652493
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-10-05 19:25:11 +0700

    customer order details get and show part 2

--- src/components/dashboard/OrderDetails.jsx
+++ src/components/dashboard/OrderDetails.jsx
@@ -1,17 +1,40 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getOrderDetails } from "../../features/orders/orderSlice";
+import { get_order_details } from "../../store/reducers/orderReducer";
 
 const OrderDetails = () => {
   const { orderId } = useParams();
   const dispatch = useDispatch();
 
+  const { userInfo } = useSelector((state) => state.auth);
+  const { myOrder } = useSelector((state) => state.order);
+
   useEffect(() => {
-    dispatch(getOrderDetails(orderId));
+    dispatch(get_order_details(orderId));
   }, [orderId]);
 
-  return <div>OrderDetails</div>;
+  return (
+    <div className="bg-white p-5">
+      <h2>
+        #{myOrder._id}, <span className="pl-1">{myOrder.date}</span>
+      </h2>
+      Deliver To: {myOrder.shippingInfo?.address}
+      <p>
+        <span className="bg-blue-100 text-blue-100 text-xs font-medium mr-2 py-2 rounded">
+          Home
+        </span>
+        <span className="text-slate-600 text-sm">
+          {myOrder.shippingInfo?.address}
+          {myOrder.shippingInfo?.province}
+          {myOrder.shippingInfo?.city}
+        </span>
+      </p>
+      <p className="text-slate-600 text-md font-semibold">
+        Email To: {userInfo.email}
+      </p>
+    </div>
+  );
 };
 
 export default OrderDetails;
--- src/store/reducers/orderReducer.js
+++ src/store/reducers/orderReducer.js
@@ -82,8 +82,12 @@ export const orderReducer = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(get_orders.fulfilled, (state, { payload }) => {
+    builder
+    .addCase(get_orders.fulfilled, (state, { payload }) => {
       state.myOrders = payload.orders;
+    })
+    .addCase(get_order_details.rejected, (state, { payload }) => {
+      state.myOrder = payload.order;
     });
   },
 });
